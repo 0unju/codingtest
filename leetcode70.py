cd5e5c9539679b22b14bae92feab7538f01c6d03
EunJu <121041665+0unju@users.noreply.github.com>
2022-12-27 22:09:02 +0900

    Update leetcode70.py

--- leetcode70.py
+++ leetcode70.py
@@ -1,1 +1,36 @@
-# Code Submitted
+# Code Submitted -> Time Limit
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        half = n//2
+        ret, new = [], []
+
+        for i in range(n, -1, -1):
+            for j in range(0, half+1):
+                l = [1] * i + [2] * j
+                if (sum(l) == n) :
+                    ret.extend(list(permutations(l, len(l))))
+
+        for value in ret:
+            if value not in new:
+                new.append(value)
+
+        return(len(new))
+      
+# Better Code
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+       
+        ways=[1,2];
+        
+        for i in range(2,n):
+            ways.append(ways[i-1]+ways[i-2])
+         
+        return ways[n-1]
